6eb13f05c817df1e897cc81898eccccca47d2a69
Suzanne Frush <suzanne.frush@gmail.com>
2025-10-15 21:11:18 -0400

    Update ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,21 @@
-{
-  "extends": "next/core-web-vitals",
-  "rules": {
-    "react/no-unescaped-entities": "off"
-  }
-}
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
+];
+
+export default eslintConfig;
